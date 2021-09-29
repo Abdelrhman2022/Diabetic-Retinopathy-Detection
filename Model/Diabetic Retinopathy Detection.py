fc9c859c460e962fb647a13d32a9a6811b6d5a33
Abdel-Rhman Ragab <56741841+Abdelrhman2022@users.noreply.github.com>
2021-09-29 18:59:29 +0200

    Last version 28/9/2021

---
+++ Model/Diabetic Retinopathy Detection.py
@@ -0,0 +1,16 @@
+from tensorflow import keras
+import keras.preprocessing.image as image
+import numpy as np
+
+# Recreate the exact same model, including its weights and the optimizer
+model = keras.models.load_model('/content/drive/MyDrive/data/LastModel.h5')
+
+def get_predictions(img):
+    test_img = image.load_img(img, target_size = (128, 128, 3))
+    test_img = image.img_to_array(test_img)
+    test_img = np.expand_dims(test_img, axis = 0)
+    pred = model.predict(test_img)
+    return  np.argmax(pred)
+
+img ='/content/drive/MyDrive/data/1008_right.jpeg'
+get_predictions(img)
